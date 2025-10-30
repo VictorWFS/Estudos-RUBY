7b7f67136b884376f0085d4bd978c921f8674172
VictorWFS <victorwfs900@gmail.com>
2025-10-30 12:27:29 -0300

    feat: simples desafio de projeto biblioteca com ruby

---
+++ DesafioBiblioteca.rb
@@ -0,0 +1,12 @@
+
+
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite a sua idade: "
+idade = gets.chomp.to_i
+
+print "Seu nome é #{nome} #{sobrenome} e você tem #{idade} anos!"
